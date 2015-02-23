c8b0556bac30035f30f2c39b39db676495b5dd48
Stephan Maximilian Huber <hello@stephanmaximilianhuber.com>
2015-02-23 21:12:22 +0100

    First working drupal installation

--- app/index.js
+++ app/index.js
@@ -1,14 +1,23 @@
 'use strict';
-var yeoman = require('yeoman-generator');
+var generators = require('yeoman-generator');
 var chalk = require('chalk');
-var yosay = require('yosay');
 var updateNotifier = require('update-notifier');
+var yosay = require('yosay');
 var _ = require('underscore');
 var fse = require('fs-extra-promise');
-require('shelljs/global');
+var shell = require('shelljs');
 
 
-module.exports = yeoman.generators.Base.extend({
+module.exports = generators.Base.extend({
+
+  constructor: function () {
+    generators.Base.apply(this, arguments);
+
+    this.options = {
+      fabalicious: true,
+    };
+  },
+
   initializing: function () {
     // Check for updates.
     var pkg = require('../package.json');
@@ -18,7 +27,7 @@ module.exports = yeoman.generators.Base.extend({
     // Check for multibasebox.
     if (!fse.existsSync('./projects')){
       this.log(chalk.red('You must be in your multibasebox folder. Multibasebox not installed? We will create a generator for that! Until that please follow the instructions here: github.com/stmh/multibasebox'));
-      exit(1);
+      shell.exit(1);
     }
 
     // Check requirements.
@@ -28,75 +37,86 @@ module.exports = yeoman.generators.Base.extend({
       drush: 'brew install drush',
       // @TODO: Need a check for pyyaml.
       //pyyaml: 'pip install pyyaml',
-    }
+    };
     _.each(requirements, function(value, key){
-      if (!which(key)) {
+      if (!shell.which(key)) {
         console.log('Sorry, this script requires ' + key + '. Install it on a Mac using: ' + value);
-        exit(1);
+        shell.exit(1);
       }
+    });
+
+  },
+
+  _getPaths : function(projectName) {
+    var base = process.env.PWD;
+    return {
+      'base': base,
+      'tools': base + '/projects/' + projectName + '/_tools',
+      'project': base + '/projects/' + projectName
+    };
+  },
+
+  _installFabalicious : function() {
+    if (!this.options.fabalicious) {
+      return false;
+    }
+
+    this.log('Installing fabalicious');
+  },
+
+  _installDrupal : function() {
+
+
+
+    var paths = this._getPaths(this.answer.name);
+    var that = this;
+
+    this.log('Installing Drupal');
+
+    // Check if the paths.project exists already.
+    if (fse.existsSync(paths.project)){
+      this.log(chalk.red('Project exists already.'));
+      shell.exit(1);
+    }
+
+    // Create paths.project.
+    fse.mkdirsAsync(paths.tools).then(function(){
+      // Commmands should be configurable.
+      var commands = {
+        gitInit: '(cd ' + paths.project + '; git init)',
+        fabalicious: '(cd ' + paths.project + ' ; git submodule add https://github.com/stmh/fabalicious.git ' + paths.tools + '/fabalicious)',
+        symlinkFabalicious: '(cd ' + paths.project + '; ln -s _tools/fabalicious/fabfile.py fabfile.py)',
+        drupaldocker: '(cd ' + paths.project + '; git submodule add https://github.com/stmh/drupal-docker.git ' + paths.tools + '/docker)',
+        drupalDownload: 'drush dl drupal --destination=' + paths.project + ' --drupal-project-rename=public'
+      };
+
+      // Loop through commands.
+      _.each(commands, function(value, key){
+        value = value + ' > /dev/null 2>&1';
+        shell.exec(value, function(code, output, list) {
+          that.log(chalk.green('Running install task for: ' + key));
+        });
+      });
+
+      that.fs.copyTpl(
+        that.templatePath('drupal/_fabfile.yaml'),
+        that.destinationPath('projects/' + that.answer.name + '/fabfile.yaml'),
+        { name: that.answer.name }
+      );
+      that.fs.copyTpl(
+        that.templatePath('drupal/_gitignore'),
+        that.destinationPath('projects/' + that.answer.name + '/.gitignore'),
+        { name: that.answer.name }
+      );
     });
   },
 
-  // Why do i have to pass this?
-  writing: {
-    drupal: function (answer, that) {
-      var base = process.env.PWD
-      var publicPath = base + '/projects/' + answer.name + '/public';
-      var toolsPath = base + '/projects/' + answer.name + '/_tools';
-      var projectPath = base + '/projects/' + answer.name;
 
-      // Check if the projectPath exists already.
-      if (fse.existsSync(projectPath)){
-        that.log(chalk.red('Project exists already.'));
-        exit(1);
-      }
-
-      // Create projectPath.
-      fse.mkdirsAsync(toolsPath).then(function(){
-        // Commmands should be configurable.
-        var commands = {
-          gitInit: '(cd ' + projectPath + '; git init)',
-          fabalicious: '(cd ' + projectPath + ' ; git submodule add https://github.com/stmh/fabalicious.git ' + toolsPath + '/fabalicious)',
-          drupaldocker: '(cd ' + projectPath + '; git submodule add https://github.com/stmh/drupal-docker.git ' + toolsPath + '/docker)',
-          drupalDownload: 'drush dl drupal --destination=' + projectPath + ' --drupal-project-rename=public'
-        };
-
-        // Loop through commands.
-        _.each(commands, function(value, key){
-          value = value + ' > /dev/null 2>&1';
-          exec(value, function(code, output, list) {
-            that.log(chalk.green('Running install task for: ' + key));
-          });
-        });
-
-        // @TODO: Copy fabfile FAILS!
-        that.fs.copyTpl(
-          that.templatePath('_fabfile.yaml'),
-          that.destinationPath(projectPath + '/fabfile.yaml'),
-          { name: answer.name }
-        );
-        console.log(projectPath + '/fabfile.yaml');
-
-      });
-
-      // TODO: Executed to early.
-      that.log(chalk.green('Mega! Project is ready!'));
-    },
-
-    wordpress: function () {
-    },
-
-    middleman: function () {
-    },
-
-    simple: function () {
-    }
-  },
 
   // Prompt for options.
   prompting: function () {
-    var done = this.async();
     var that = this;
+    var cb = this.async();
 
     // Say yo!
     this.log(yosay(
@@ -104,37 +124,36 @@ module.exports = yeoman.generators.Base.extend({
     ));
 
     this.prompt([{
+      type: 'input',
       name: 'name',
       message: 'Name of your project',
       validate: function(input) {
-        // why?
-        var done = this.async();
-        if (/\W/.test(input) || input.length === 0){
-          done("You need to provide letters, numbers and underscores only");
-          return;
-        }
-        // We are done.
-        done(true);
+        return input.match(/^[a-zA-Z0-9_]+$/)? true : 'project name can only contain letters, numbers and underscores';
       },
     }, {
       name: 'projectType',
       type: 'list',
       message: 'What kind of docker container do you wish?',
-      choices: ['drupal', 'wordpress', 'middleman', 'simple webserver']
+      choices: [
+        'Drupal',
+        'Wordpress',
+        'Middleman',
+        { 'value': 'SimpleWebserver', 'name': 'Simple Webserver'}
+      ]
     }], function (answer) {
-      // @TODO: that.writing.{variable} -> function call.
-      if (answer.projectType == 'drupal') {
-        that.writing.drupal(answer, that);
-      }else{
-        that.log(chalk.red('Not supported right now.'));
-      }
+      that.answer = answer;
+      cb();
     });
   },
 
-  // Example for options.
+
   install: function () {
-    this.installDependencies({
-      skipInstall: this.options['skip-install']
-    });
+    var funcName = '_install' + this.answer.projectType;
+    if (this[funcName]) {
+      this[funcName]();
+    }
+    else {
+      this.log(chalk.red(this.answer.projectType + ' not implemented yet!'));
+      }
   }
 });
